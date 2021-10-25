cf7569792a39b71c337011a3c3775912624ddd39
cmq <arsnowyizumi@gmail.com>
2021-10-25 23:37:57 +0800

    美术资源

---
+++ Assets/Scripts/CustomPlayerController/CustomCameraInput.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace CustomPlayerController
+{
+    public abstract class CustomCameraInput: MonoBehaviour
+    {
+        public abstract float GetHorizontalCameraInput();
+        public abstract float GetVerticalCameraInput();
+    }
+}

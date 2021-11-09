21d582e35fa6cacef799eeba8aa22f41af7441ae
surface <2468004642@qq.com>
2021-11-09 14:10:42 +0800

    basse commit test

--- Assets/Scripts/UI/SyetemUI/HealthBarUI.cs
+++ Assets/Scripts/UI/SyetemUI/HealthBarUI.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+// Base branch commit test 
 public class HealthBarUI : MonoBehaviour
 {
    public GameObject HealthBarPrefab;
